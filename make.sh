7d69293cb611df99bcbf6a6bde0331bf798d9b8f
iam <iam4722202468@gmail.com>
2015-06-15 21:37:27 -0400

    Added compile only for linux to make script

--- make.sh
+++ make.sh
@@ -3,6 +3,10 @@ if [ "$1" = "" ] || [ "$1" = "--all" ] ; then
 	cd ../ && mv build/game .
 	cd ./build_win && make
 	cd ../ && mv build_win/game.exe .
+elif [ "$1" = "--linux" ]
+then
+	cd ./build && make
+	cd ../ && mv build/game .
 elif [ "$1" = "--clean" ]
 then
 	rm ./build/*.o
